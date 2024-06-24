d23ca710a236831780077cea64d5fa7670071628
Pranav <44663510+Pranavb333@users.noreply.github.com>
2024-06-24 12:04:11 +0530

    Enabled CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import handleError from "./middleware/error.middleware";
 import { authenticate } from "./middleware/auth.middleware";
 import usersRouter from "./users/users.controller";
 
 const app = express();
+app.use(cors({ origin: true, credentials: true }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
