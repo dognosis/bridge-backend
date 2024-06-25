50d3b017128b50525e85cd9dfd80d1d4b4659810
Pranav <44663510+Pranavb333@users.noreply.github.com>
2024-06-25 13:25:24 +0530

    Improved async router error handling and error logging

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,15 +2,37 @@ import { NextFunction, Request, Response } from "express";
 import HttpExpception from "../exceptions/HttpException";
 
 const handleError = (
-  error: HttpExpception,
+  error: HttpExpception | Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const status = error.status || 500;
-  const message = error.message || "Something went wrong";
+  if (error instanceof HttpExpception) {
+    const status = error.status;
+    const message = error.message;
+
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  console.error(
+    "---Unexpected-Error---",
+    "Rquest",
+    req,
+    "Response",
+    res,
+    "Error message",
+    error.message,
+    "Error Stack",
+    error.stack,
+    "---Unexpected-Error---"
+  );
+
+  const status = 500;
+  const message = "Something went wrong";
 
   res.status(status).json({ status, message });
+  return;
 };
 
 export default handleError;
